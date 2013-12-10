42dcfc121670eef7cd1086709a6ee89ef1e97a59
Barry Mullan <mullan@gmail.com>
2013-12-10 15:22:44 -0500

    first

---
+++ App.js
@@ -0,0 +1,68 @@
+var app = null;
+
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+    launch: function() {
+        //Write app code here
+        app = this;
+        var store = this.getSnapshotStore();
+
+        var viewport = Ext.create('Ext.Viewport');
+
+        viewport.add({
+             xtype: 'rallygrid',
+             store : store,
+             columnCfgs: [
+                 {dataIndex:'_UnformattedID',text:"ID"},
+                 {text:"From State", renderer : function(v,m,r) {
+                 	return r.get("_PreviousValues").State;
+                 }},
+                 {dataIndex:'State',text:"State"},
+                 {dataIndex:'Priority',text:"Priority"},
+                 {dataIndex:'Severity',text:"Severity"},
+                 {dataIndex:'_ValidFrom',text:"Valid From", renderer: function(value) {
+                 	return Rally.util.DateTime.fromIsoString(value);
+                 }}
+             ]
+        });
+
+    },
+
+    getSnapshotStore : function() {
+
+        var that = this;
+        var fetch = ['ObjectID','_UnformattedID','State','Priority','Severity','_ItemHierarchy','_TypeHierarchy','_ValidFrom','_PreviousValues'];
+        var hydrate = ['_TypeHierarchy','State','Priority','Severity','_PreviousValues'];
+        
+        var find = {
+                '_TypeHierarchy' : { "$in" : ["Defect"]} ,
+                '_ProjectHierarchy' : { "$in": app.getContext().getProject().ObjectID },
+                'State' : "Open",
+                '_PreviousValues.State' : { "$ne" : null}
+        };
+
+        var storeConfig = {
+            find : find,
+            autoLoad : true,
+            pageSize:1000,
+            limit: 'Infinity',
+            fetch: fetch,
+            hydrate: hydrate,
+            listeners : {
+                scope : this,
+                load: function(store, snapshots, success) {
+                    console.log("completed snapshots:", snapshots.length);
+                }
+            }
+        };
+
+        var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', storeConfig);
+
+        return snapshotStore;
+
+    }
+
+
+});
